e51cd54e1fb12ba02815c0d5b69adfd1d0a978d3
Aaditya2302 <aadityaaggarwal475@gmail.com>
2026-06-19 00:57:20 +0530

    update cors

--- server/server.js
+++ server/server.js
@@ -19,10 +19,20 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      console.log("Blocked Origin:", origin);
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true
   })
 );
+
 
 app.use(express.json());
 
